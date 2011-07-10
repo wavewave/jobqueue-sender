ef059f43a23717761bb27e4382c29f5dca3bec98
ianwookim <ianwookim@gmail.com>
2011-07-10 06:45:31 +0000

    Priority can be set.

--- src/HEP/Automation/JobQueue/Sender/Job.hs
+++ src/HEP/Automation/JobQueue/Sender/Job.hs
@@ -13,23 +13,26 @@ import Data.Aeson.Encode
 import Control.Concurrent (threadDelay)
 
 import HEP.Automation.JobQueue.Sender.Type
-import HEP.Automation.MadGraph.Dataset.Set20110707set2
+import HEP.Automation.MadGraph.Dataset.Set20110707set1
 
 jobqueueSend :: Url -> IO ()
 jobqueueSend url = do 
-  let jobdetails = map (flip EventGen webdavdir) eventsets
+  let jobdetails = map (flip (MathAnal "tev_reco") webdavdir) eventsets
   putStrLn $ "sending " ++ show (length eventsets) ++ " jobs"
-  mapM_ (\x -> sendJob url x >> threadDelay 1000000) jobdetails
+  mapM_ (\x -> sendJob url x NonUrgent >> threadDelay 1000000) jobdetails
 
-sendJob :: Url -> JobDetail -> IO () 
-sendJob url jobdetail = do 
-  withManager $ \manager -> do  -- manager <- newManager 
-    requesttemp <- parseUrl (url ++ "/queue")
+sendJob :: Url -> JobDetail -> JobPriority -> IO () 
+sendJob url jobdetail prior = do 
+  withManager $ \manager -> do  
+    requesttemp <- case prior of 
+                     Urgent    -> parseUrl (url ++ "/queue/1")
+                     NonUrgent -> parseUrl (url ++ "/queue/0")
     let json = encode $ toAeson jobdetail 
     let myrequestbody = RequestBodyLBS json 
-    let requestpost = requesttemp { method = methodPost, 
-                                    requestHeaders = [ ("Content-Type", "text/plain") ], 
-                                    requestBody = myrequestbody } 
+    let requestpost = requesttemp 
+          { method = methodPost
+          , requestHeaders = [ ("Content-Type", "text/plain") ]
+          , requestBody = myrequestbody } 
     r <- httpLbs requestpost manager 
     putStrLn $ show r 
 
